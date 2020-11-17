b8b0b3d72d3312b5c422851e24b7213cb8c10c78
James Hsu <70932507+jameshsu1125@users.noreply.github.com>
2020-11-17 12:43:32 +0800

    fix Don2Json

--- src/lib/LESCA/Unit/Dom2Json.js
+++ src/lib/LESCA/Unit/Dom2Json.js
@@ -1,23 +1,21 @@
 const $ = require('jquery');
+const findDom = (tar, dat) => {
+	const self = this;
+	let t = $(tar).children('div'),
+		s = tar.className;
+	dat[s] = {};
+	if (t.length > 0 && s) {
+		t.each(function () {
+			let len = $(this).children('div').length;
+			let sty = this.className;
+			let txt = this.innerText;
+			if (len == 0) dat[s][sty] = txt;
+			else self.findDom(this, dat[s]);
+		});
+	}
+};
 module.exports = function (dom) {
 	let data = {};
-
-	this.findDom = (tar, dat) => {
-		const self = this;
-		let t = $(tar).children('div'),
-			s = tar.className;
-		dat[s] = {};
-		if (t.length > 0 && s) {
-			t.each(function () {
-				let len = $(this).children('div').length;
-				let sty = this.className;
-				let txt = this.innerText;
-				if (len == 0) dat[s][sty] = txt;
-				else self.findDom(this, dat[s]);
-			});
-		}
-	};
-
-	this.findDom(dom, data);
+	findDom(dom, data);
 	return data;
 };
